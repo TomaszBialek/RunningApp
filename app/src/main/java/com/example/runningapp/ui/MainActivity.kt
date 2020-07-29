653062a6dda3533c4748c13bb9843f02823a8000
Tomasz Białek <bialek.tomasz225@gmail.com>
2020-07-29 20:55:36 +0200

    UI

--- app/src/main/java/com/example/runningapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/runningapp/ui/MainActivity.kt
@@ -1,8 +1,8 @@
-package com.example.runningapp
+package com.example.runningapp.ui
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.example.runningapp.R
 import com.example.runningapp.db.RunDao
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
