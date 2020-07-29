9abdb7fb1593edf682fd0cf0dbe9c146425da5dd
Tomasz Białek <bialek.tomasz225@gmail.com>
2020-07-29 19:53:43 +0200

    Hilt

---
+++ app/src/main/java/com/example/runningapp/di/AppModule.kt
@@ -0,0 +1,29 @@
+package com.example.runningapp.di
+
+import android.content.Context
+import androidx.room.Room
+import com.example.runningapp.db.RunningDatabase
+import com.example.runningapp.other.Constants.RUNNING_DATABASE_NAME
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Singleton
+
+@Module
+@InstallIn(ApplicationComponent::class)
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideRunningDatabase(@ApplicationContext context: Context) = Room.databaseBuilder(
+        context,
+        RunningDatabase::class.java,
+        RUNNING_DATABASE_NAME
+    ).build()
+
+    @Singleton
+    @Provides
+    fun provideRunDao(database: RunningDatabase) = database.getRunDao()
+}
